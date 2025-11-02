b5a6ba835a6dc3cb572002af69f2329a9263a1a8
Iddrisu_Sulemana <iddrisusulemana665@gmail.com>
2025-11-02 07:45:40 +0000

    Fix TypeScript type errors

--- prodev-mobile-app-0x00/app/index.tsx
+++ prodev-mobile-app-0x00/app/index.tsx
@@ -1,18 +1,15 @@
 import { Text, View } from "react-native";
 
-export default function TabOneScreen() {
+export default function Index() {
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#fff",
       }}
     >
-      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-        ** First App Created**
-      </Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
